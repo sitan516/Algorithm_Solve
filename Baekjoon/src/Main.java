8a9b169de4a6106bea707dccb9c26208e8edd071
sitan516 <sitan516@github.com>
2021-03-25 17:38:05 +0900

    +1

--- Baekjoon/src/Main.java
+++ Baekjoon/src/Main.java
@@ -4,29 +4,27 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main{
+	
+	static int[] arr;
+	
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
-		int num[] = new int[10];
-		int mul = 1;
-		for (int i = 0; i < 3; i++) {
-			mul *= Integer.parseInt(br.readLine());
-		}
 		
-		while(mul >0) {
-			num[mul % 10]++;
-			mul /= 10;
-		}
-		for (int i = 0; i < 10; i++) {
-			System.out.println(num[i]);
-		}
+		
+		
+		System.out.println(sb);
+		
+		
 	}
 	
 	
 	
 	
 }
+
+
 
