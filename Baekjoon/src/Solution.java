b8080375838d39ca0e003eb3a1be34e1ea7fff12
sitan516 <sitan516@github.com>
2021-04-15 12:40:26 +0900

    t

--- Baekjoon/src/Solution.java
+++ Baekjoon/src/Solution.java
@@ -6,7 +6,11 @@ import java.util.*;
 
 public class Solution {
 
-	
+	static int N;
+	static int W;
+	static int H;
+	static int min;
+	static int[][] tempMap;
 	
 	public static void main(String[] args) throws Exception {
 
@@ -20,90 +24,23 @@ public class Solution {
 			
 			st = new StringTokenizer(br.readLine());
 				
-			int N = Integer.parseInt(st.nextToken());
-			int X = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			W = Integer.parseInt(st.nextToken());
+			H = Integer.parseInt(st.nextToken());
+			min = Integer.MAX_VALUE;
+			int[][] map = new int[H][W];
 			
-			int map[][] = new int[N][N];
-			
-			for (int r = 0; r < N; r++) {
+			for (int r = 0; r < H; r++) {
 				st = new StringTokenizer(br.readLine());
-				for (int c = 0; c < N; c++) {
+				for (int c = 0; c < W; c++) {
 					map[r][c] = Integer.parseInt(st.nextToken());
 				}
 			}
 			
-			int cnt = 0;
+			// DFS 방식을 이용함
+			DFS(map, 0); 
 			
-			// 높이가 바뀌면 낮은쪽 높이가 연속으로 x개 존재해야함
-			
-			for (int r = 0; r < N; r++) {
-				int serial = 1;
-				int last = map[r][0];
-				int underFlag = 0;
-				
-				boolean cntFlag = true;
-				for (int c = 1; c < N; c++) {
-					if(underFlag > 0 && serial == X) {
-						serial = 0;
-						underFlag--;
-					}
-					if(map[r][c] == last) {
-						serial++;
-					} else if(map[r][c] > last) {
-						if(serial < X * (map[r][c] - last)) {
-							cntFlag = false;
-							break;
-						}
-						serial = 1;
-						last = map[r][c];
-					} else if(map[r][c] < last) {
-						if(underFlag > 0) {
-							cntFlag = false;
-							break;
-						}
-						serial = 1;
-						underFlag = last - map[r][c];
-						last = map[r][c];
-					}
-				}
-				if(underFlag > 0 && serial < X) cntFlag = false;
-				if(cntFlag) cnt++;
-			}
-			
-			for (int c = 0; c < N; c++) {
-				int serial = 1;
-				int last = map[0][c];
-				int underFlag = 0;
-				boolean cntFlag = true;
-				for (int r = 1; r < N; r++) {
-					if(underFlag > 0 && serial == X) {
-						serial = 0;
-						underFlag--;
-					}
-					if(map[r][c] == last) {
-						serial++;
-					} else if(map[r][c] > last) {
-						if(serial < X * (map[r][c] - last)) {
-							cntFlag = false;
-							break;
-						}
-						serial = 1;
-						last = map[r][c];
-					} else if(map[r][c] < last) {
-						if(underFlag > 0) {
-							cntFlag = false;
-							break;
-						}
-						serial = 1;
-						underFlag = last - map[r][c];
-						last = map[r][c];
-					}
-				}
-				if(underFlag > 0 && serial < X) cntFlag = false;
-				if(cntFlag) cnt++;
-			}
-			
-			sb.append("#").append(testCase).append(" ").append(cnt).append("\n").append("\n");
+			sb.append("#").append(testCase).append(" ").append(min).append("\n");
 			
 			
 		
@@ -114,6 +51,81 @@ public class Solution {
 		
 
 		
+	}
+
+	private static void DFS(int[][] map, int cnt) {
+		
+		// 다 떨어뜨렸으면 검사하기
+		if(cnt == N) {
+			int brick = 0;
+			for (int r = 0; r < H; r++) {
+				for (int c = 0; c < W; c++) {
+					if(map[r][c] != 0) brick++;
+				}
+			}
+			min = Math.min(brick, min);
+			return;
+		}
+		
+		tempMap = new int[H][W];
+		// 하나씩 벽돌 떨어뜨리기
+		for (int c = 0; c < W; c++) {
+			for (int r = 0; r < H; r++) {
+				tempMap[r] = map[r].clone();
+			}
+			
+			// 벽돌 떨어뜨리기
+			dropBrick(c, tempMap);
+			// 벽돌 정리하기
+			arrangeBrick(tempMap);
+			
+			DFS(tempMap, cnt+1);
+		}
+	}
+
+	private static void arrangeBrick(int[][] map) {		
+		for (int c = 0; c < W; c++) {
+			int stackPointer = H-1;
+			for (int r = H-1; r >= 0; r--) {
+				if(map[r][c] != 0) {
+					map[stackPointer--][c] = map[r][c];
+				}
+			}
+			for (int r = stackPointer; r >= 0; r--) {
+				map[r][c] = 0;
+			}
+		}
+	}
+
+	private static void dropBrick(int c, int[][] map) {
+		for (int r = 0; r < H; r++) {
+			if(map[r][c] != 0) {
+				bomb(r,c,map);
+				break;
+			}
+		}
+	}
+
+	private static void bomb(int r, int c, int[][] map) {
+		
+		int size = map[r][c];
+		map[r][c] = 0;
+		
+		int rStart = (r - size + 1) >= 0 ? (r - size + 1) : 0;
+		int rEnd = (r + size - 1) < H ? (r + size - 1) : H-1;
+		int cStart = (c - size + 1) >= 0 ? (c - size + 1) : 0;
+		int cEnd = (c + size - 1) < W ? (c + size - 1) : W-1;
+		
+		for (int dr = rStart; dr <= rEnd; dr++) {
+			if(map[dr][c] != 0) {
+				bomb(dr, c, map);
+			}
+		}
+		for (int dc = cStart; dc <= cEnd; dc++) {
+			if(map[r][dc] != 0) {
+				bomb(r, dc, map);
+			}
+		}
 	}
 	
 }
